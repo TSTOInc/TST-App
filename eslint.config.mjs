ae352885d52db1f27fc2af610465ce02242d49eb
Angel Dev <75772159+Angel-999@users.noreply.github.com>
2025-08-16 15:26:12 -0700

    disable rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  
+  // Add this to disable the rule globally
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
