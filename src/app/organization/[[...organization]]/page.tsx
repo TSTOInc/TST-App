de74e1aeb372276b5f8e4f0dee5de018f3939fd5
Angel Dev <haloloha220066@gmail.com>
2026-02-11 00:40:35 -0800

    ui layout update fix organization

--- src/app/organization/[[...organization]]/page.tsx
+++ src/app/organization/[[...organization]]/page.tsx
@@ -4,8 +4,9 @@ const UserProfilePage = () => <OrganizationProfile appearance={
     { 
         elements: { 
             rootBox: "w-full h-full", 
-            cardBox: "h-full rounded-t-none border-none max-w-[100vw] grid-cols-[calc(var(--clerk-spacing,_1rem)_*_14.25)_3fr]", 
-            pageScrollBox: "bg-background", 
+            cardBox: "w-full h-full rounded-t-none border-none max-w-[100vw] grid-cols-[calc(var(--clerk-spacing,_1rem)_*_14.25)_1fr] bg-red-500", 
+            pageScrollBox: "bg-background rounded-none", 
+            scrollBox: "rounded-none",
             navbar: "bg-sidebar",
             navbarMobileMenuRow: "bg-sidebar",
         } 
